e5ee85b667b3ed81ab6783c81334240fa53c38b3
NATANAEL PEREIRA DOS SANTOS <natanael.psantos1@senacsp.edu.br>
2022-09-16 19:55:50 -0300

    Incluindo validatoken

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,12 +1,13 @@
 const productsRoutes = require('express').Router();
 const products = require('../controller/products.controller')
+const {validaToken} = require('../middlewares/auth')
 
 
-productsRoutes.get('/all', products.findAll)
-productsRoutes.get('/show/:id', products.findOne)
-productsRoutes.post('/create', products.create)
-productsRoutes.put('/update', products.update)
-productsRoutes.delete('/delete/:id', products.delete)
+productsRoutes.get('/all', validaToken, products.findAll)
+productsRoutes.get('/show/:id', validaToken, products.findOne)
+productsRoutes.post('/create', validaToken, products.create)
+productsRoutes.put('/update', validaToken, products.update)
+productsRoutes.delete('/delete/:id', validaToken, products.delete)
 
 
 
